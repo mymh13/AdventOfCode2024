1442b921861a18320a0ac81a66273ddaee76af23
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-12-02 12:53:02 +0100

    intialization of the basic console app

---
+++ Advent_of_code_2024/AdventOfCodeMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_code_2024
+{
+    internal class AdventOfCodeMenu
+    {
+        internal static void RunMenu()
+        {
+            Console.WriteLine("Please enter the number of the day you would like to run");
+
+            // Here I want a switch statement menu to run the different days and to validate the input
+        }
+    }
+}
---
+++ Advent_of_code_2024/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Advent_of_code_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wow, 2024! Each code puzzle will have their own file, this will just be a link to a menu");
+            AdventOfCodeMenu.RunMenu();
+        }
+    }
+}
